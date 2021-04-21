7e7a9f582f49c8ef80c31fd4379ec8403c6f1b44
tamanobi <tamanobi@gmail.com>
2021-04-22 00:35:03 +0900

    feat: upload dropbox

---
+++ upload.py
@@ -0,0 +1,11 @@
+from dropbox import Dropbox
+from pathlib import Path
+import settings
+
+db = Dropbox(settings.ACCESS_TOKEN)
+folder = Path(settings.SRC_FOLDER)
+for path in folder.glob("**/*"):
+    if path.is_file() and not path.name.endswith(".part"):
+        print(path.name)
+        with open(path, 'rb') as f:
+            db.files_upload(f.read(), f"{settings.DST_FOLDER}{path.name}")
