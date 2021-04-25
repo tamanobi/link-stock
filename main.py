e9d8ee8e17fb10de4cf1d031e8efd9d7a03abc6d
tamanobi <tamanobi@gmail.com>
2021-04-25 23:59:57 +0900

    style: black

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ import sqlite3
 while True:
     time.sleep(1)
 
-    con = sqlite3.connect('image.db')
+    con = sqlite3.connect("image.db")
     cur = con.cursor()
-    cur.execute('''
+    cur.execute(
+        """
         CREATE TABLE IF NOT EXISTS saved
             (
                 id integer
                 , url text
                 , created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
             )
-    ''')
+    """
+    )
 
     ids = []
     req = urllib.request.Request(url="https://cryptic-wildwood-50649.herokuapp.com/get")
@@ -31,9 +33,13 @@ while True:
         if res.status != 200:
             exit(1)
         records = json.loads(res.read())["record"]
-        ids = [(rec["id"], rec.get("url")) for rec in records if rec.get("url") is not None and "?tags" not in rec.get("url")]
+        ids = [
+            (rec["id"], rec.get("url"))
+            for rec in records
+            if rec.get("url") is not None and "?tags" not in rec.get("url")
+        ]
 
-    cur.execute('SELECT id, url FROM saved')
+    cur.execute("SELECT id, url FROM saved")
     subete = [(x[0], x[1]) for x in cur.fetchall()]
     atarashii = list(set(ids) - set(subete))
     if len(atarashii) > 0:
@@ -42,12 +48,22 @@ while True:
         url = a[1].replace("/ja/", "/")
         GALLERY_DL = "/home/tamanobi/.anyenv/envs/pyenv/versions/3.8.5/bin/gallery-dl"
         cmd = f"{GALLERY_DL} --config ./gallery-dl.conf {url}"
-        proc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+        proc = subprocess.run(
+            cmd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        )
         print(cmd)
         print(proc.stderr)
 
-        cur.execute('''
+        cur.execute(
+            """
             INSERT INTO saved (id, url) VALUES (?, ?)
-        ''', [int(a[0]), a[1]])
+        """,
+            [int(a[0]), a[1]],
+        )
         con.commit()
     con.close()
--- upload.py
+++ upload.py
@@ -7,5 +7,5 @@ folder = Path(settings.SRC_FOLDER)
 for path in folder.glob("**/*"):
     if path.is_file() and not path.name.endswith(".part"):
         print(path.name)
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             db.files_upload(f.read(), f"{settings.DST_FOLDER}{path.name}")
