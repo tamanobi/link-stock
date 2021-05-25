68cb15af75bc03f2d227832e6537c4f5f300f27f
tamanobi <tamanobi@gmail.com>
2021-05-25 19:59:56 +0900

    refactor: use exception

--- ext.py
+++ ext.py
@@ -1,6 +1,13 @@
 import urllib.request
 import json
 
+
+class APIError(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = "APIサーバーエラー"
+        super().__init__(*args, **kwargs)
+
+
 class LinkStockAPI:
     @classmethod
     def get(cls):
@@ -8,9 +15,9 @@ class LinkStockAPI:
         req = urllib.request.Request(url="https://cryptic-wildwood-50649.herokuapp.com/get")
         with urllib.request.urlopen(req) as res:
             if res is None:
-                exit(1)
+                raise APIError()
             if res.status != 200:
-                exit(1)
+                raise APIError()
             records = json.loads(res.read())["record"]
             ids = [
                 (rec["id"], rec.get("url"))
