e763592841ea4684d819b58026173b7dc61782ad
tamanobi <tamanobi@gmail.com>
2021-06-01 00:27:52 +0900

    refactor: main

--- main.py
+++ main.py
@@ -18,33 +18,34 @@ def normalize_url_list(list_) -> list:
     return [(id_, normalize_url(url)) for id_, url in list_]
 
 
-while True:
-    gdl = GalleryDL()
-    with stock_db_context() as db:
-        db.create_table_if_not_exists()
+if __name__ == "__main__":
+    while True:
+        gdl = GalleryDL()
+        with stock_db_context() as db:
+            db.create_table_if_not_exists()
 
-        local_ids = db.get_all()
-        remote_ids = LinkStockAPI.get()
+            local_ids = db.get_all()
+            remote_ids = LinkStockAPI.get()
 
-        remote_ids = [(id_, normalize_url(url)) for id_, url in remote_ids]
+            remote_ids = [(id_, normalize_url(url)) for id_, url in remote_ids]
 
-        have_not_saved_ids = list(set(remote_ids) - set(local_ids))
-        have_not_saved_ids = normalize_url_list(have_not_saved_ids)
-        if len(have_not_saved_ids) > 0:
-            print(have_not_saved_ids)
-        for id_, url in have_not_saved_ids:
-            url = normalize_url(url)
-            proc = gdl.run(url)
-            print(proc.stderr)
+            have_not_saved_ids = list(set(remote_ids) - set(local_ids))
+            have_not_saved_ids = normalize_url_list(have_not_saved_ids)
+            if len(have_not_saved_ids) > 0:
+                print(have_not_saved_ids)
+            for id_, url in have_not_saved_ids:
+                url = normalize_url(url)
+                proc = gdl.run(url)
+                print(proc.stderr)
 
-            # import numpy as np
-            # import pdqhash
-            # from PIL import Image
-            # im = Image.open("deadbeef.jpg")
-            # np.asarray(im)
-            # im_np = np.asarray(im)
-            # hash_vector, quality = pdqhash.compute(im_np)
+                # import numpy as np
+                # import pdqhash
+                # from PIL import Image
+                # im = Image.open("deadbeef.jpg")
+                # np.asarray(im)
+                # im_np = np.asarray(im)
+                # hash_vector, quality = pdqhash.compute(im_np)
 
-            db.insert(int(id_), url)
-            db.commit()
-    time.sleep(1)
+                db.insert(int(id_), url)
+                db.commit()
+        time.sleep(1)
